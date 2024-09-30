53e28ec6ef54de0619ee265ef5b30ce04cc19665
Alishba-Khan02 <ak6445268@gmail.com>
2024-09-30 14:25:50 +0500

    Hero-section using NextJs

---
+++ src/components/profile.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import developer from "@/public/images/developer.jpeg";
+export default function Profile() {
+  return (
+    <div className="Main flex mt-20 ml-8">
+      <div className="sub-section leading-10 font-bold text-lg">
+        Hey,
+        <br />
+        This side <span className="text-red-600">Alishba Khan</span>
+        <br />
+        <span className="text-sky-500">I am a Frontend Developer</span>
+        <br />
+        <p>
+          I am responsible for planning and developing software solutions and web
+          <br /> applications, supporting and maintaining a company's websites and digital
+          products.
+        </p>
+      </div>
+      
+      {/* Add Image here */}
+        <Image
+          src={developer} 
+          alt="Full stack developer"
+          width={400}
+          height={200}
+        />
+      </div>
+  );
+}
